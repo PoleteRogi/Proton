b52fa97fa5c0315c6c2d1fb4d6803ed4ce43a622
PoleteRogi <hola@soypol.com>
2021-01-04 19:43:55 +0100

    Created Proton Compiler

--- Proton/Program.cs
+++ Proton/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Diagnostics;
 using Microsoft.CSharp;
 using System.CodeDom.Compiler;
 using System.IO;
@@ -34,13 +33,59 @@ namespace Proton
                     Application.Run(form);
                     break;
                 case "compile":
+                    Console.WriteLine("ANTES0");
+                    
                     CSharpCodeProvider csc = new CSharpCodeProvider(new Dictionary<string, string>() { { "CompilerVersion", "v4.0" } });
-                    CompilerParameters parameters = new CompilerParameters(new[] { "mscorlib.dll", "System.Core.dll" }, Environment.CurrentDirectory + @"\CompiledProtonApp.exe", true);
+                    CompilerParameters parameters = new CompilerParameters(new[] { "mscorlib.dll", "System.dll", "System.Core.dll" }, Environment.CurrentDirectory + @"\CompiledProtonApp.exe", false);
                     parameters.GenerateExecutable = true;
-                    string source = string.Format(File.ReadAllText(Application.StartupPath + @"\compile\.compile"), Environment.CurrentDirectory);
+                    string source = "" +
+                        "using System;" +
+                        "using System.Collections.Generic;" +
+                        "using System.Linq;" +
+                        "using System.Threading.Tasks;" +
+                        "using System.Diagnostics;" +
+                        //"using System.Diagnostics.Process;" +
+                        "namespace Proton {" +
+                        "   static class Program {" +
+                        "       static void Main() {" +
+                        "           Process process = new System.Diagnostics.Process();" +
+                        "           process.StartInfo.FileName = \"proton\";" +
+                        "           process.StartInfo.Arguments = \". user\";" +
+                        "           process.StartInfo.WorkingDirectory = Environment.CurrentDirectory;" +
+                        "           process.Start(); " +
+                        "           " +
+                        "       } " +
+                        "   } " +
+                        "}";
+                    //Console.WriteLine(source);
+                    Console.WriteLine("ANTES1");
                     CompilerResults results = csc.CompileAssemblyFromSource(parameters, source);
+                    Console.WriteLine("ANTES2");
+                    if (results.Errors.HasErrors)
+                    {
+                        Console.WriteLine("ANTES3");
+                        results.Errors.Cast<CompilerError>().ToList().ForEach(error => Console.WriteLine(error));
+                        Console.WriteLine("ANTES4");
+                    }
+                    else
+                    {
+                        Console.WriteLine("ANTES5");
+                        Console.WriteLine("Compiled succefully");
+                    }
+                    Console.WriteLine("ANTES6");
                     break;
+
             }
+            Console.WriteLine("ANTES7");
+        }
+
+        static void Algo()
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            process.StartInfo.FileName = "proton";
+            process.StartInfo.Arguments = ". user";
+            process.StartInfo.WorkingDirectory = Environment.CurrentDirectory;
+            process.Start();
         }
     }
 }
